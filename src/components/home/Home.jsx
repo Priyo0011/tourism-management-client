f6f5c877b808bc6887e4cbfc483a237117a083f6
Priyo0011 <priyotosh0011@gmail.com>
2024-04-25 22:01:18 +0600

    first commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "./Banner";
+
+
+const Home = () => {
+    return (
+        <div className="max-w-[1600px] mx-auto">
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
